78592edef4520e016ed46df06c0b018e34c925ba
yuli-r <iuliana.rotaru234@gmail.com>
2023-05-03 16:29:28 +0100

    Set up repository

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|webp|svg|ico)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
